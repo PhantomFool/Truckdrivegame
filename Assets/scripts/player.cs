ac310ddff1b244ae2d53d2e5ccf072c724fb61f6
PhantomFool <ultramariosuper64@gmail.com>
2025-06-21 08:49:49 -0400

    i changed my editor to rider so let me cook

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float movespeed;
